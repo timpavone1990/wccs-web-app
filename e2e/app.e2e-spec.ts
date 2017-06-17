f2516ddc0a2a204e205c5928c0bc3cb32c179e78
Tim Gremplewski <tim.gremplewski@gmail.com>
2017-06-17 11:37:01 +0200

    Updated angular js

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { WctsWebAppPage } from './app.po';
 
-describe('wcts-web-app App', function() {
+describe('wcts-web-app App', () => {
   let page: WctsWebAppPage;
 
   beforeEach(() => {
     page = new WctsWebAppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
   });
 });
